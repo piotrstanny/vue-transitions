160957c4560638f22b44ea7cbd41bc11174d9f9f
Piotr Stanny <piotr.stanny@gmail.com>
2020-05-01 09:28:22 +0100

    Info alert showing on button click using v-if

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,9 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Animations</h1>
-
+                <hr>
+                <button class="btn btn-primary" @click="show = !show">Show Alert</button>
+                <div class="alert alert-info" role="alert" v-if="show">This is info alert!</div>
             </div>
         </div>
     </div>
@@ -13,12 +15,14 @@
     export default {
         data() {
             return {
-
+                show: false
             }
         }
     }
 </script>
 
 <style>
-
+.container, .alert, hr {
+    margin-top: 30px;
+}
 </style>
